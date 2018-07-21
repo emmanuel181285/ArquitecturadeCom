5b0082365fd3b7d05afaea7a2eb2e9f30c23ee43
emmanuel181285 <alon.r.c@gmail.com>
2018-07-21 10:04:52 -0600

    RAM clock signal sync

--- MICRO/RAM.vhd
+++ MICRO/RAM.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-USE ieee.numeric_std.ALL;
+use ieee.numeric_std.ALL;
+use ieee.std_logic_unsigned.ALL;
 
 
 entity RAM is
@@ -9,28 +10,30 @@ port(
  RAM_ADDR: in std_logic_vector(6 downto 0); 
  RAM_DATA_IN: in std_logic_vector(7 downto 0);
  RAM_WR: in std_logic; 
- RAM_CLOCK: in std_logic; 
+ --RAM_CLOCK: in std_logic; 
+ CLK : in std_logic;
  RAM_DATA_OUT: out std_logic_vector(7 downto 0) 
 );
 end RAM;
 
 architecture A_RAM of RAM is
 
-type RAM_ARRAY is array (0 to 15 ) of std_logic_vector (7 downto 0);
+type RAM_ARRAY is array (0 to 127 ) of std_logic_vector (7 downto 0);
 
 signal RAM : RAM_ARRAY;
 
   
 begin
-process(RAM_CLOCK)
+process(CLK)
 			begin
-				if(rising_edge(RAM_CLOCK)) then
-				if(RAM_WR='0') then 
-            RAM(to_integer(unsigned(RAM_ADDR))) <= RAM_DATA_IN;
-            --RAM_DATA_OUT <= RAM(to_integer(unsigned(RAM_ADDR)));
-			end if;
-		end if;
+				if(rising_edge(CLK)) then
+								if (RAM_WR='1') then
+								RAM_DATA_OUT <= RAM(to_integer(unsigned(RAM_ADDR)));
+			               elsif (RAM_WR='0') then
+								RAM(to_integer(unsigned(RAM_ADDR))) <= RAM_DATA_IN;
+								
+			               end if;
+			   end if;
 end process;
- 
- RAM_DATA_OUT <= RAM(to_integer(unsigned(RAM_ADDR)));
+
 end A_RAM;
